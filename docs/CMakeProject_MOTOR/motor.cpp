9d485254e62a770f5b3c12ce2fd228a71b9fe9c7
Fogsanches <kirkago312@gmail.com>
2025-04-26 10:25:12 +0300

    Update motor.cpp

--- docs/CMakeProject_MOTOR/motor.cpp
+++ docs/CMakeProject_MOTOR/motor.cpp
@@ -1,12 +1,6 @@
-﻿// CMakeProject1.cpp: определяет точку входа для приложения.
-//
+#include "motor.h"
+#include <iostream>
 
-#include "CMakeProject1.h"
-
-using namespace std;
-
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
+void Motor::run() {
+    std::cout << "Motor is running..." << std::endl;
 }
